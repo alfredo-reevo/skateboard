e3347d97d885eaa1577101fb21a3da83ad3d6db3
Alfie Reeves <47298517+Kopperr@users.noreply.github.com>
2021-06-25 11:44:43 +0100

    Playermodel animation controller
    Retrieves triggered keys - in this case, when "w" is held, it loops the moving animation for the playermodel. When the "w" key is released, the playermodel returns to the idle animation.

---
+++ AnimController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            anim.Play("Skateboarding-Move");
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            anim.Play("Skateboarding-idle");
+        }
+    }
+}
